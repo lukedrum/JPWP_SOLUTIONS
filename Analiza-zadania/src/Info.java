4165c5015c81ac35080f4b0260c862529adc8509
lukedrum <32622061+lukedrum@users.noreply.github.com>
2020-05-26 10:51:26 +0200

    Add task 1.1 solution

--- Analiza-zadania/src/Info.java
+++ Analiza-zadania/src/Info.java
@@ -27,17 +27,20 @@ public class Info {
                 Document document = connect.get();
                 //Find all <a> tags
                 //TODO 1.1 Znajdź w documencie wszystkie znaczniki <a ...>, zapisz je jako allA
+                // Znaczniki a jako elements
+                Elements allA = document.select("a");
                 //Search through all found <a> tags
                 for (Element elem : allA) {
                     //Compare given phrase with text from Elements
-                    if () { //TODO 1.1 Znajdź w tekście wszystkie wystąpienia phrase (przydatna może okazac się funkcja text())
+                    //TODO 1.1 Znajdź w tekście wszystkie wystąpienia phrase (przydatna może okazac się funkcja text())
+                    if (elem.text().toLowerCase().contains(phrase.toLowerCase())) {
                         //Handle relative addresses
                         if (elem.attr("href").startsWith("/")) {
                             //TODO 1.1 Utwórz hiperłącze na wyszukanym tekście, który umieścisz jako h2. Warto opakować hiperłącza w containery
-
+                            site.append("<div class = 'wrapper'><a href='").append(url).append(elem.attr("href")).append("<h2>").append(elem.text()).append("</h2></a></div>");
                         } else {
                             //TODO 1.1 Witryna gov.pl nie jest skora do współpracy. Dodaj adres url do znacznika <a href> przed przypisaniem adresu, pod którym znajduje się artykuł
-
+                            site.append("<div class = 'wrapper'><a href='").append(elem.attr("href")).append("<h2>").append(elem.text()).append("</h2></a></div>");
                         }
                     }
                 }
@@ -63,15 +66,19 @@ public class Info {
                 Document document = connect.get();
                 //Find all <a> tags
                 //TODO 1.1 Znajdź w documencie wszystkie znaczniki <a ...>, zapisz je jako allA
+                Elements allA = document.select("a");
                 //Search through all found <a> tags
                 for (Element elem : allA) {
                     //Compare given phrase with text from Elements
-                    if () { //TODO 1.1 Znajdź w tekście wszystkie wystąpienia 'koronawirus' (przydatna może okazac się funkcja text())
+                    //TODO 1.1 Znajdź w tekście wszystkie wystąpienia 'koronawirus' (przydatna może okazac się funkcja text())
+                    if (elem.text().toLowerCase().contains(("koronawirus").toLowerCase())) {
                         if (elem.attr("href").startsWith("/")) {
                             //TODO 1.1 Dopisz adres URL jeśli strona zawiera adresowanie względne, a następnie zapisz tytuł
                             //TODO 1.1 artykułu i odnośnik do niego do zmiennej newsletter
+                            newsletter.append("\n\nZe strony: ").append(elem.text()).append(url).append(elem.attr("href"));
                         } else {
                             //TODO 1.1 Dopisz tytuł artykułu i odnośnik do niego do zmiennej newsletter
+                            newsletter.append("\n\nZe strony: ").append(elem.text()).append(elem.attr("href"));
                         }
                     }
                 }
